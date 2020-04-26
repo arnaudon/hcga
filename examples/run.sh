45d2c28b4b757358399d5b69e48f572b53e8f742
alexis arnaudon <alexis.arnaudon@epfl.ch>
2020-04-26 15:21:33 +0200

    Revert "SBM dataset"
    This reverts commit ae95107e36020ed672651c9a52c53417c86ca2e1.

--- examples/run.sh
+++ examples/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-<<<<<<< Updated upstream
 #need this is a virtualenv called hcga is used
 #set -e
 #module purge all
@@ -18,7 +17,3 @@ python3 run_feature_extraction.py $1
 # then run classification (this can be run separately, 
 # as the features are saved from the previous script)
 python3 run_classification.py $1
-=======
-export OMP_NUM_THREADS=1  # set to one to prevent numpy to run in parallel
-hcga extract_features $1 -m fast -n 2 -sl advanced #--runtimes
->>>>>>> Stashed changes
